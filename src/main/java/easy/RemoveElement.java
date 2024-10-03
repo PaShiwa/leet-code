29c4f87bed17d2075743e3f289b58b1718b9d72f
Pawan Shiwakoti <pawanshiwakoti45@gmail.com>
2024-10-03 16:37:23 -0400

    Remove Element solved from easy branch!

---
+++ src/main/java/easy/RemoveElement.java
@@ -0,0 +1,26 @@
+package src.main.java.easy;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int value){
+        int i =0;
+        int index = 0;
+        while (i<nums.length){
+
+            if (nums[i]!=value){
+                nums[index]= nums[i];
+                index++;
+            }
+            i++;
+        }
+        return index ;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3,2,1,5,3,4};
+        int value = 3;
+        RemoveElement removeElement = new RemoveElement();
+        int result = removeElement.removeElement(nums, value);
+        System.out.println(result);
+    }
+}
