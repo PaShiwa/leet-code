253686660a121ef1ea7689a30ba2132c7bf2ca9b
Pawan Shiwakoti <pawanshiwakoti45@gmail.com>
2025-01-07 20:03:21 -0700

    Soved Two Sum problem in O(n) time!

--- src/main/java/easy/TwoSum.java
+++ src/main/java/easy/TwoSum.java
@@ -1,27 +1,31 @@
 package src.main.java.easy;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
-        int n = nums.length;
-
-        for (int i=0; i<n; i++){
-            int complement = target - nums[i];
-            if (numMap.containsKey(complement)){
-                return new int[] {numMap.get(complement),i};
-            }
-            numMap.put(nums[i],i);
+    public static void main(String[] args) {
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        int[] twoSumArray = twoSum(numbers,target);
+        if (twoSumArray!=null) {
+            System.out.println("Two sum: " + Arrays.toString(twoSumArray));
+        }else {
+            System.out.println("No solution exists!");
         }
-       return new int[]{};
     }
 
-    public static void main(String[] args) {
-        int[] nums= {3,4,2,7};
-        int target =9;
-        int[] resultIndex = new TwoSum().twoSum(nums,target);
-        System.out.println(Arrays.toString(resultIndex));
+    static int[] twoSum(int[] numbers,int target){
+        Map<Integer,Integer> numMap = new HashMap<>();
+        int n = numbers.length;
+        for(int i =0;i<n; i++){
+            int complement = target-numbers[i];
+            if(numMap.containsKey(complement)){
+                return new int[] {numMap.get(complement),i};
+            }
+            numMap.put(numbers[i],i);
+        }
+        return null;
     }
 }
