938188e83010be32163d8ada73ff90a950e6c7fa
PawShiwa <pawanshiwakot45@gmail.com>
2025-08-31 21:56:18 -0700

    solved anagram problem using: 1. HasMap and frequency count 2. Frequency Array

--- src/main/java/array/TwoSum.java
+++ src/main/java/array/TwoSum.java
@@ -4,17 +4,20 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
-    public int[] twoSum(int[] intArray, int target){
+    public int[] twoSum(int[] intArray, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i< intArray.length; i++){
-            int compliment = target-intArray[i];
-            if(map.containsKey(compliment)){
-                return new int[]{map.get(compliment), i};
+        for (int i = 0; i < intArray.length; i++) {
+            int complement = target - intArray[i];
+            if (map.containsKey(complement)) {
+                int idx1 = map.get(complement);
+
+                return idx1 < i ? new int[]{idx1, i} : new int[]{i, idx1};
             }
-            map.put(intArray[i],i);
+            map.put(intArray[i], i);
         }
         return null;
     }
+
     public static void main(String[] args) {
         int[] intArray = {2,6,3,4,5,1};
         int target = 5;
