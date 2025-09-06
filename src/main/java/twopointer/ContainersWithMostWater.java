d634301c7689d3ee5f8ff9fac0ddea6aa8982009
PawShiwa <pawanshiwakot45@gmail.com>
2025-09-05 22:42:24 -0700

    Solved the containers with most water problem using two pointers.

--- src/main/java/twopointer/ContainersWithMostWater.java
+++ src/main/java/twopointer/ContainersWithMostWater.java
@@ -1,2 +1,25 @@
-package LeetCode.src.main.java.twopointer;public class ContainersWithMostWater {
+package LeetCode.src.main.java.twopointer;
+
+public class ContainersWithMostWater {
+    int maxWater(int[] height){
+        int max =0;
+        int left =0;
+        int right = height.length -1;
+
+        while (left <right) {
+            int currentMax = Math.min(height[left], height[right]) * (right-left);
+            max = Math.max(max,currentMax);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] waterHeight = {1,8,6,2,5,4,8,3,7};
+        System.out.println("Max: " + new ContainersWithMostWater().maxWater(waterHeight));
+    }
 }
